53c3faabf80844b57da37b1366115ba0e286864f
snifles <nissehanel09@cock.li>
2021-04-04 08:57:00 +0200

    non-working part removed. will now also be incorrectly indented

--- dcbot.py
+++ dcbot.py
@@ -86,29 +86,29 @@ async def on_message(message):
         
 
 
-    if message.content.startswith('!userinfo'):
-        args = message.content.split(' ')
-        if len(args) == 2:
-            member: Member = discord.utils.find(lambda m: args[1] in m.name, message.guild.members)
-            if member:
-                embed = discord.Embed(title='Userinfo für {}'.format(member.name),
+    #if message.content.startswith('!userinfo'):
+        #args = message.content.split(' ')
+        #if len(args) == 2:
+            #member: Member = discord.utils.find(lambda m: args[1] in m.name, message.guild.members)
+            #if member:
+               # embed = discord.Embed(title='Userinfo für {}'.format(member.name),
                        description='Dies ist die Userinfo für den User {}'.format(member.mention),
                        color =0x22a7f0)
-                embed.add_field(name ='Server beigetreten', value=member.joined_at.strftime('%d/%m/Y, %H:%M:%S'),
+                #embed.add_field(name ='Server beigetreten', value=member.joined_at.strftime('%d/%m/Y, %H:%M:%S'),
                        inline=True)
 
-                embed.add_field(name='Discord beigetreten', value=member.created_at.strftime('%d/%m/Y, &H:%M:%S'),
+                #embed.add_field(name='Discord beigetreten', value=member.created_at.strftime('%d/%m/Y, &H:%M:%S'),
                        inline=True)
 
-                for role in member.roles:
-                    if not role.is_default():
+                #for role in member.roles:
+                 #   if not role.is_default():
                        rollen += '{} \r\n'.format(role.mention)
-                if rollen:
-                    embed.add_field(name='Rollen', value=rollen, inline=True)
+                #if rollen:
+                #    embed.add_field(name='Rollen', value=rollen, inline=True)
 
-                embed.set_thumbnail(url=member.avatar_url)
-                embed.set_footer(text='Ich vin ein EmbedFooter.')
-                await message.channel.send(embed=embed)
+               # embed.set_thumbnail(url=member.avatar_url)
+                #embed.set_footer(text='Ich vin ein EmbedFooter.')
+                #await message.channel.send(embed=embed)
 
 
 
